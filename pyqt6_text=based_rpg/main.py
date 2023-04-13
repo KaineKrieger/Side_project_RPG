241d6630a4abd76ff7605fee8c5bac6d82c78648
KaineKriegar <kainekriegar1447@gmail.com>
2023-04-13 15:29:01 -0700

    editing with help from winika

---
+++ pyqt6_text=based_rpg/main.py
@@ -0,0 +1,64 @@
+import text_blocks
+
+
+# just gunna do a lot of psudo code for now to shape it
+
+path = "start"
+introduction = text_blocks.introduction
+initial_choice = text_blocks.initial_choice
+
+
+def menu_basics(event, question):
+    print(menu)
+    answer = int(input(question))
+
+if __name__ == "__main__":
+
+if path == "start":
+    event = introduction
+    question = initial_choice
+    menu_basics(event, question)
+    path == question
+
+    if path == 1:
+
+
+
+    elif path == 2:
+
+
+
+
+    elif path == 3:
+
+
+
+
+    elif path == 4:
+
+
+
+    elif path == 5:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+    print("error, please restart code.")
+
+
+
