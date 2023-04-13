46635beb0d979d2a6130a60abd463cc5cbdef0f9
KaineKriegar <kainekriegar1447@gmail.com>
2023-04-13 14:43:42 -0700

    creating text_blocks.py

---
+++ pyqt6_text=based_rpg/text_blocks.py
@@ -0,0 +1,27 @@
+introduction == ("You walk into the dark room, the soft sounds of the .outside world fading behind you as you enter and the door shuts loudly behind you sending a tremor throughout the room. You notice that there are numerous exits too the room and objects in the room
+
+ In the far left corner theres a doorway, coated in moss and vines, gently swaying in a unfelt but heard wind
+
+ In the far right corner theres a table pressed against the corner, a weird circular white and blue object shines and moves on its own, barely floating above the table, thrumming in a minor chord over and over again with each slow rotation.
+
+ In the right corner front he entrance you just walked through theres a stairway leading downward into darkness, soft whispers come from the dark.
+
+ In the left corner from the enterene there is a spiral staircase leading upwards towards light, water can be heard through the hole in the ceiling it would lead you through, the stairs themselves slick with condensation
+
+ In the center of the room is a slightly raised platform with a pedestal on it, on the pedestal is a large dusty book
+
+")
+
+
+initial_choice == "what do you choose? (enter a number corresponding with an option)
+    1 Enter the mossy doorway
+
+2 Enter the dark stairwell
+
+3 Enter the spiral staircase
+
+4 touch the magical sphere
+
+5 Investigate the book
+
+"
