bdffb335437e1db571920f01719018aa7330690a
KaineKriegar <kainekriegar1447@gmail.com>
2023-06-02 14:55:08 -0700

    im so confused

--- pyqt6_text=based_rpg/main.py
+++ pyqt6_text=based_rpg/main.py
@@ -1,27 +1,21 @@
 import text_blocks
 
 
-# just gunna do a lot of psudo code for now to shape it
-
-path = "start"
-introduction = text_blocks.introduction
-initial_choice = text_blocks.initial_choice
-answer = 1
-
 def menu_basics(event, question):
-    print(event)
-    return answer == int(input(question))
+        print(event)
+        answer = str(input(question))
+        return answer
 
-if __name__ == "__main__":
 
+
+def initial_pathway(path):
     if path == "start":
         event = introduction
         question = initial_choice
         path == menu_basics(event, question)
-
-        if path == 1:
+  
+        if path == "1":
             print(text_blocks.path_1)
-
 
 
         if path == 2:
@@ -29,10 +23,8 @@ if __name__ == "__main__":
 
 
 
-
         if path == 3:
             print(text_blocks.path_3)
-
 
 
 
@@ -63,6 +55,14 @@ if __name__ == "__main__":
 
     else:
         print("error, please restart code.")
+
+
+if __name__ == "__main__":
+    path = "start"
+    introduction = text_blocks.introduction
+    initial_choice = text_blocks.initial_choice
+    answer = 1
+    initial_pathway(path)
 
 
 
