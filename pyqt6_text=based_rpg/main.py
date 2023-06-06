be2749fd3a2d45eb01546af73609299c732c59bf
KaineKriegar <kainekriegar1447@gmail.com>
2023-06-06 14:30:48 -0700

    mrwinikkahelpedandsomehowfixedallofmyproblemswth

--- pyqt6_text=based_rpg/main.py
+++ pyqt6_text=based_rpg/main.py
@@ -7,51 +7,25 @@ def menu_basics(event, question):
         return answer
 
 
-
 def initial_pathway(path):
     if path == "start":
         event = introduction
         question = initial_choice
-        path == menu_basics(event, question)
+        path = menu_basics(event, question)
   
         if path == "1":
             print(text_blocks.path_1)
-
-
         if path == 2:
             print(text_blocks.path_2)
-
-
 
         if path == 3:
             print(text_blocks.path_3)
 
-
-
         if path == 4:
             print(text_blocks.path_4)
 
-
         if path == 5:
             print(text_blocks.path_5)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     else:
         print("error, please restart code.")
--- pyqt6_text=based_rpg/text_blocks.py
+++ pyqt6_text=based_rpg/text_blocks.py
@@ -44,11 +44,33 @@ path_1 = ("You pass through the mossy doorway,",
         " the other is a cracked statue of what looks to be a philosopher,",
         " both are leaning against each other and in the center of the room",
         " appears to be a pressure plate in which one statue is meant to sit,",
-        " which statue do you choose to place on the plate?"
+        " which statue do you choose to place on the plate?",
+        " 1 the broken philospher or 2 the perfect warrior"
 )
 
 
-path_2 = ("")
+path_2 = ("Against your better judgment… I suppose curiosity or stupidity,",
+        " I’d say bravery but that implies more stubbornness than intelligence."
+        " and i appreciate you, you enter the dark abyss of the staircase,",
+        " stepping down soft whispering sound fills your ears as the darkness",
+        " suffocates your eyesight, relying solely on touch hearing and smell",
+        " from now on you descend into the darkness, and then all of the sudden",
+        " it all turns green and gray. Seemingly the dungeon itself has granted",
+        " you dark vision!  How wonderful. And now that you can see,",
+        " you can notice that the walls around you move slowly under your hands,",
+        " they quiver and dance in response to each step you take,",
+        " continuing down the moving corridor it opens up into a large circular room",
+        " with numerous doorways, however all are locked, in the center of the room",
+        " is a large pillar connecting to the ceiling with two large artworks",
+        " on each side of it, both with a small pressure plate on them,",
+        " on one side there is a depiction of a large scale war of two sides,",
+        " one purple the other orange, clashing violently. And on the other side",
+        " of the pillar you see a large artwork depicting a blending of two",
+        " different colors, one side a bright yellow and the other a",
+        " dark blue slowly blending together like a horizon during the",
+        " moments right before dawn.")
+
+path_2_question = "1 the clashing tide or, 2 the merging sky"
 
 
 path_3 = ("")
@@ -63,7 +85,8 @@ path_4 = ("You walk over too the table, the artifact continues",
         " and then suddenly nothing, no sound no sensation no light no dark",
         " no noise no silence no feeling of clothes no breathing no nothing.",
         " For nothing would mean the absence of something thus creating something,",
-        " sheer pure nothingness. What do you do?"
+        " sheer pure nothingness. What do you do?",
+        " 1 scream into the uncaring void or, 2 try to move your body"
 )
 
 
